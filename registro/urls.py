be296bcf247780c2943a9bcfc5b2d3885e12ea1b
osm3rr <osm3rr@mail.com>
2024-12-14 10:37:09 -0400

    Login

--- registro/urls.py
+++ registro/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
-from registro.views import SignUpView
+from registro.views import SignUpView,Home
+
 
 urlpatterns=[
     path("accounts/",include('django.contrib.auth.urls')),
-    path("",SignUpView.as_view(),name="signup")
+    path("signup/",SignUpView.as_view(),name="signup"),
+    path("",Home,name="Home")
 ]
--- registro/views.py
+++ registro/views.py
@@ -3,9 +3,14 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+
 class SignUpView(CreateView):
 
     form_class=UserCreationForm
     success_url=reverse_lazy("login")
     template_name="registration/signup.html"
+
+def Home(request):
+
+    return render(request,"home.html",)
 
